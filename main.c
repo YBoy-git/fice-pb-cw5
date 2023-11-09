ff9e8acc69a24b9e8c63264d641b4ae9eb23a995
YBoy <antonparfonov@gmail.com>
2023-11-09 14:33:27 +0200

    Basic UI implemented

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_STRING_LENGTH 100
+#define MAX_STRINGS_QUANTITY 100
 
 void clearIB()
 {
@@ -40,7 +44,7 @@ void swapStrings(char *string1, char *string2)
     size_t string1Length = strlen(string1);
     size_t string2Length = strlen(string2);
     size_t maxLength = (string1Length > string2Length) ? string1Length : string2Length;
-    char *temp = malloc(maxLength + 1);
+    char *temp = malloc((maxLength + 1) * sizeof(char));
     strcpy(temp, string1);
     strcpy(string1, string2);
     strcpy(string2, temp);
@@ -80,37 +84,106 @@ void printStrings(char **strings, size_t stringsQuantity)
     return;
 }
 
-void test()
+bool isInputValid(char *input, size_t maxLength)
 {
-    size_t maxStringLength = 10;
-    size_t stringsQuantity = 3;
-    char **strings = malloc(stringsQuantity);
-    for (int i = 0; i < stringsQuantity; i++)
+    size_t inputLength = strlen(input);
+    if (inputLength >= maxLength - 1)
     {
-        strings[i] = malloc(maxStringLength + 1);
+        char *lastCharacter = &(input[inputLength - 1]);
+        if (*lastCharacter != '\n')
+        {
+            printf("The input is too big, try again.\n");
+            clearIB();
+            return false;
+        }
     }
-    strcpy(strings[0], "Привіт!");
-    strcpy(strings[1], "Байбай.");
-    strcpy(strings[2], "Хелло))");
+    return true;
+}
 
-    sortStrings(strings, stringsQuantity, 1);
+void getString(char *string, size_t maxInputLength)
+{
+    // to include \n and \0
+    maxInputLength += 2;
 
-    printStrings(strings, stringsQuantity);
+    char input[maxInputLength];
+    do
+    {
+        // max string length + \n + \0 in ideal string
+        fgets(input, maxInputLength, stdin);
+    } while (!isInputValid(input, maxInputLength));
 
+    input[strlen(input) - 1] = '\0';
+    strcpy(string, input);
+
+    return;
+}
+
+// Returns quantity of input strings
+size_t getStrings(char **strings)
+{
+    size_t stringsQuantity = 0;
+    char *input = malloc((MAX_STRING_LENGTH) * sizeof(char));
+
+    do
+    {
+        if (stringsQuantity > 0)
+        {
+            printf("You've entered %zu string(s)\n", stringsQuantity);
+        }
+        printf("Enter a string or press ENTER to stop input\n");
+
+        getString(input, MAX_STRING_LENGTH);
+        printf("\n");
+
+        if (strlen(input) != 0)
+        {
+            strings[stringsQuantity] = malloc((strlen(input) + 1) * sizeof(char));
+            strcpy(strings[stringsQuantity], input);
+            stringsQuantity++;
+        }
+    } while (strlen(input) != 0 && stringsQuantity < 100);
+
+    return stringsQuantity;
+}
+
+void freeStrings(char **strings, size_t stringsQuantity)
+{
     for (int i = 0; i < stringsQuantity; i++)
     {
         free(strings[i]);
-        printf("freed\n");
     }
-    printf("freed every string\n");
-
     free(strings);
+}
+
+void UI()
+{
+    char **strings = malloc(MAX_STRINGS_QUANTITY * sizeof(char *));
+    size_t stringsQuantity = getStrings(strings);
+    if (stringsQuantity == 0)
+    {
+        printf("I can't work with empty arrays\n");
+        return;
+    }
+    sortStrings(strings, stringsQuantity, 1);
+    printStrings(strings, stringsQuantity);
+    freeStrings(strings, stringsQuantity);
+    return;
+}
+
+void endless(void (*function)())
+{
+    do
+    {
+        function();
+        printf("Press ENTER to continue and type any key to exit\n");
+    } while (getchar() == '\n');
 
     return;
 }
 
 int main()
 {
-    test();
+    endless(UI);    
+
     return 0;
 }
